a56c605ec51b413d9413560866bc36392133e25b
Zhang Zhe <38390381+Zhang3zhe@users.noreply.github.com>
2019-03-05 19:51:04 +0800

    Update 004.py

--- 100examples/004.py
+++ 100examples/004.py
@@ -1,12 +1,20 @@
+# Script based python 3.7
+
 def isLeapYear(y):
-    return (y%400==0 or (y%4==0 and y%100!=0))
-DofM=[0,31,28,31,30,31,30,31,31,30,31,30]
-res=0
-year=int(input('Year:'))
-month=int(input('Month:'))
-day=int(input('day:'))
-if isLeapYear(year):
-    DofM[2]+=1
+    if (y%4==0 and y%100!=0) or y%400==0:
+        return True
+    else:
+        return False
+
+Table = ((0, 31,28,31,30,31,30,31,31,30,31,30,31),(0, 31,29,31,30,31,30,31,31,30,31,30,31))
+
+date = input("Input a date:\n")
+date_s = date.split(' ')
+year = int(date_s[0])
+month = int(date_s[1])
+day = int(date_s[2])
+sum = 0
 for i in range(month):
-    res+=DofM[i]
-print(res+day)
+    sum += Table[isLeapYear(year)][i]
+sum += day
+print(sum)
