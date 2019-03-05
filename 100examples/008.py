46f39727e1612b8cbaa55212e88d71fe9f13fb53
Zhang Zhe <38390381+Zhang3zhe@users.noreply.github.com>
2019-03-05 21:13:29 +0800

    Update 008.py

--- 100examples/008.py
+++ 100examples/008.py
@@ -1,4 +1,8 @@
+# Script based python 3.7
+
 for i in range(1,10):
     for j in range(1,i+1):
-        print('%d*%d=%2ld '%(i,j,i*j),end='')
-    print()
+        print(j,'*',i,'=',i*j,end='\t')
+    print('\n')    
+    
+    
