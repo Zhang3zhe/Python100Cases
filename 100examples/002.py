b3bee4e2b213b3f5d96554aa889e828ffbc51ab7
Zhang Zhe <38390381+Zhang3zhe@users.noreply.github.com>
2019-03-05 17:57:02 +0800

    Update 002.py

--- 100examples/002.py
+++ 100examples/002.py
@@ -1,15 +1,18 @@
-profit=int(input('Show me the money: '))
-bonus=0
-thresholds=[100000,100000,200000,200000,400000]
-rates=[0.1,0.075,0.05,0.03,0.015,0.01]
-for i in range(len(thresholds)):
-    if profit<=thresholds[i]:
-        bonus+=profit*rates[i]
-        profit=0
-        break
-    else:
-        bonus+=thresholds[i]*rates[i]
-        profit-=thresholds[i]
-bonus+=profit*rates[-1]
-print(bonus)
+# Script based python 3.7
 
+data   = int(input('Input the money (w)\n'))
+
+if data>0 and data<=10:
+    bonus = data * 0.1
+elif data>10 and data<=20:
+    bonus = 10*0.1 + (data-10)*0.075
+elif data > 20 and data <= 40:
+    bonus = 10*0.1 + 10*0.075 + (data-20)*0.05
+elif data > 40 and data <= 60:
+    bonus = 10*0.1 + 10*0.75 + 20*0.05 + (data-40)*0.03
+elif data>60 and data<=100:
+    bonus = 10 * 0.1 + 10 * 0.75 + 20 * 0.05 + 20 * 0.03 + (data-60)*0.015
+else:
+    bonus = 10 * 0.1 + 10 * 0.75 + 20 * 0.05 + 20 * 0.03 + 40*0.015 + (data-100)*0.01
+
+print(bonus)
